5fc43fb9d44a730efc0ab8ef765a34cb96fedc45
Mattéo Delucchi <matteo.dlcch@gmail.com>
2023-12-17 16:16:10 +0100

    Refactor BoardString write method to use a constant pointer and return the updated result

--- src/io.zig
+++ src/io.zig
@@ -4,41 +4,42 @@ const masks = @import("masks.zig");
 const BoardString = struct {
     string: [71]u8,
 
-    fn write(self: *BoardString, where: masks.Mask, what: u8) BoardString {
+    pub fn write(self: *const BoardString, where: masks.Mask, what: u8) BoardString {
+        var result = self.*;
         var index: u64 = 0;
         var mask: masks.Mask = 1;
 
         while (true) : (mask <<= 1) {
             if (0 < mask & where) {
-                self.string[index] = what;
+                result.string[index] = what;
             }
             index += 1;
             if (index % 9 == 8) {
                 if (index == 71) break;
-                self.string[index] = '\n';
+                result.string[index] = '\n';
                 index += 1;
             }
         }
 
-        return self.*;
+        return result;
     }
 };
 
 pub fn stringify(board: chess.Board) BoardString {
     var result = BoardString{ .string = undefined };
-    result.write(masks.full, '.');
-    result.write(board.white.pawns, 'P');
-    result.write(board.white.knights, 'N');
-    result.write(board.white.bishops, 'B');
-    result.write(board.white.rooks, 'R');
-    result.write(board.white.queens, 'Q');
-    result.write(board.white.king, 'K');
-    result.write(board.black.pawns, 'p');
-    result.write(board.black.knights, 'n');
-    result.write(board.black.bishops, 'b');
-    result.write(board.black.rooks, 'r');
-    result.write(board.black.queens, 'q');
-    result.write(board.black.king, 'k');
+    result = result.write(masks.full, '.');
+    result = result.write(board.white.pawns, 'P');
+    result = result.write(board.white.knights, 'N');
+    result = result.write(board.white.bishops, 'B');
+    result = result.write(board.white.rooks, 'R');
+    result = result.write(board.white.queens, 'Q');
+    result = result.write(board.white.king, 'K');
+    result = result.write(board.black.pawns, 'p');
+    result = result.write(board.black.knights, 'n');
+    result = result.write(board.black.bishops, 'b');
+    result = result.write(board.black.rooks, 'r');
+    result = result.write(board.black.queens, 'q');
+    result = result.write(board.black.king, 'k');
     return result;
 }
 
