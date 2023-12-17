da49b3a6d69ad740e2876346ad98e7a672e51683
Mattéo Delucchi <matteo.dlcch@gmail.com>
2023-12-17 18:35:41 +0100

    Add queenLookup function

--- src/movegen.zig
+++ src/movegen.zig
@@ -160,3 +160,7 @@ inline fn rookLookup(rook: Index, occupied: chess.Bitboard) chess.Bitboard {
 
     return col_lookup | row_lookup;
 }
+
+inline fn queenLookup(queen: Index, occupied: chess.Bitboard) chess.Bitboard {
+    return bishopLookup(queen, occupied) | rookLookup(queen, occupied);
+}
