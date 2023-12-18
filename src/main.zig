83c8cf9c0aa42d40446841f804e55ca834831f27
Mattéo Delucchi <matteo.dlcch@gmail.com>
2023-12-19 00:27:39 +0100

    Refactor main function to handle different arguments

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,29 @@ const std = @import("std");
 const debugger = @import("debugger.zig");
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+    var args = std.process.args();
 
-    const fen = args[1];
-    const depth = try std.fmt.parseInt(u64, args[2], 10);
+    _ = args.skip();
+    const fen = args.next() orelse unreachable;
+    const depth = try std.fmt.parseInt(u64, args.next() orelse unreachable, 10);
+    const info = args.next() orelse "noinfo";
 
-    try debugger.perftInfo(fen, depth);
+    if (cmp(info, "info")) {
+        try debugger.perftInfo(fen, depth);
+    } else if (cmp(info, "noinfo")) {
+        try debugger.perft(fen, depth);
+    } else {
+        unreachable;
+    }
+}
+
+fn cmp(str1: [:0]const u8, str2: [:0]const u8) bool {
+    var index: usize = 0;
+    while (true) {
+        if (str1[index] == 0) return str2[index] == 0;
+        if (str2[index] == 0) return false;
+        if (str1[index] != str2[index]) return false;
+        index += 1;
+    }
+    unreachable;
 }
