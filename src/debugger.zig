d205602bece79f3f3ba40ae85e6fc3474b96a2ef
Mattéo Delucchi <matteo.dlcch@gmail.com>
2023-12-19 00:10:31 +0100

    Add getMNodesPerSec helper function and new perft function

--- src/debugger.zig
+++ src/debugger.zig
@@ -8,10 +8,9 @@ pub fn perftInfo(fen: [*:0]const u8, depth: u64) !void {
 
     const t0 = std.time.nanoTimestamp();
     const nodes = movegen.exploreEntry(movegen.explore, board, depth, perftInfoDetails, board);
-    const dt: u64 = @intCast(std.time.nanoTimestamp() - t0);
+    const dt = std.time.nanoTimestamp() - t0;
 
-    const mn_per_100_sec: f64 = @floatFromInt(nodes * 1000 * 100 / dt);
-    const mnodes_per_s: f64 = mn_per_100_sec / 100;
+    const mnodes_per_s = getMNodesPerSec(nodes, dt);
 
     std.debug.print("{d} nodes found in {d}ms ({d:.3} MNodes/s)\n", .{ nodes, @divTrunc(dt, 1000000), mnodes_per_s });
 }
@@ -25,4 +24,21 @@ fn perftInfoDetails(board: chess.Board, depth: u64, comptime color: chess.Color,
     const dst = new.occupied() & diff;
     std.debug.print("{s}{s}: {d}\n", .{ io.maskToSquare(src), io.maskToSquare(dst), nodes });
     return nodes;
+}
+
+fn getMNodesPerSec(nodes: u64, dt: i128) f64 {
+    const mn_per_100_sec: f64 = @floatFromInt(nodes * 1000 * 100 / @as(u64, @intCast(dt)));
+    return mn_per_100_sec / 100;
+}
+
+pub fn perft(fen: [*:0]const u8, depth: u64) !void {
+    const board = try io.parse(fen);
+
+    const t0 = std.time.nanoTimestamp();
+    const nodes = movegen.exploreEntry(movegen.exploreThread, board, depth, null, null);
+    const dt = std.time.nanoTimestamp() - t0;
+
+    const mnodes_per_s = getMNodesPerSec(nodes, dt);
+
+    std.debug.print("{d} nodes found in {d}ms ({d:.3} MNodes/s)\n", .{ nodes, @divTrunc(dt, 1000000), mnodes_per_s });
 }
