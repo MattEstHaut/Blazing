b3b3713780dfbc00f7e5c9795b9a07153593cbec
Mattéo Delucchi <matteo.dlcch@gmail.com>
2023-12-18 17:20:25 +0100

    Add debugger.perftInfo function call

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,6 @@
 const std = @import("std");
+const debugger = @import("debugger.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    try debugger.perftInfo("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 5);
+}
