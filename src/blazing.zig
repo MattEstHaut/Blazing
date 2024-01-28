72ab715b0741e97ae565eee02fd13641751202b0
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-01-29 00:08:31 +0100

    Add generate function to blazing.zig

--- src/blazing.zig
+++ src/blazing.zig
@@ -4,6 +4,8 @@ const chess = @import("chess.zig");
 pub const explore = movegen.explore;
 pub const CallbackReturn = movegen.CallbackReturn;
 
+pub const generate = movegen.generate;
+
 pub const Board = chess.Board;
 pub const void_board = chess.void_board;
 
